58cce32e48fccffe11ba1f23fa39158ba3e38f5a
Seborider <mayer.sebobo@gmail.com>
2022-05-03 23:35:33 +0200

    ⚡️ Implement full functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,12 +33,27 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`
-    About to generate a password with the following:
-    Includes letters: ${this.includeLetters}
-    Includes numbers: ${this.includeNumbers}
-    Includes symbols: ${this.includeSymbols}
-    `)
-    this.password = "MY PASSWORD!"
+    const numbers = "123456789";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols ="!§$%&/()=";
+
+    let validChars = "";
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
