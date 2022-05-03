525948500adbeb58dc3b101b52ef6a4c7c391689
Seborider <mayer.sebobo@gmail.com>
2022-05-03 13:13:48 +0200

    ✨ Track onChange properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = "";
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
+    console.log(`
+    About to generate a password with the following:
+    Includes letters: ${this.includeLetters}
+    Includes numbers: ${this.includeNumbers}
+    Includes symbols: ${this.includeSymbols}
+    `)
     this.password = "MY PASSWORD!"
   }
 }
